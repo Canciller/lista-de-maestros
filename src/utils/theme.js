3ec30d3621b8fa328ce2e98a6793508a829d3f3a
canciller <lopez.ojeda.gabriel.emilio@gmail.com>
2020-02-02 13:24:51 -0600

    Renamed file

---
+++ src/utils/theme.js
@@ -0,0 +1,12 @@
+export default {
+    default: {
+        primary: 'black',
+        secondary: 'gray',
+        text: {
+            normal: 'white',
+        },
+        link: {
+            normal: 'blue',
+        },
+    },
+};
