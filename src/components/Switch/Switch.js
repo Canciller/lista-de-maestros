601035e89088fb2e1da7ea641c6df9625ba7faec
canciller <lopez.ojeda.gabriel.emilio@gmail.com>
2020-02-08 12:28:19 -0600

    Updated Switch component

--- src/components/Switch/Switch.js
+++ src/components/Switch/Switch.js
@@ -1,21 +1,128 @@
 import React from 'react';
-//import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import { withTheme } from 'components/Theme';
+import mergeStyles from 'utils/mergeStyles';
 
-import './Switch.scss';
+const Input = styled.input`
+    opacity: 0;
+    width: 0;
+    height: 0;
+`;
+
+const Slider = styled.span`
+    position: absolute;
+    cursor: pointer;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: #ccc;
+    -webkit-transition: 0.4s;
+    transition: 0.4s;
+    border-radius: ${props => `${props.borderRadius}px`};
+    ${props => props.checked && `background-color: ${props.color};`};
+
+    :before {
+        position: absolute;
+        content: '';
+        ${props => `
+            height: ${props.size - 8}px;
+            width: ${props.size - 8}px;
+        `}
+        left: 4px;
+        bottom: 4px;
+        background-color: white;
+        -webkit-transition: 0.4s;
+        transition: 0.4s;
+        border-radius: 50%;
+        ${props =>
+            props.checked &&
+            `
+                    box-shadow: 0 0 1px ${props.color};
+                    transform: translateX(${props.translate - props.size}px);
+            `}
+    }
+`;
 
 class Switch extends React.Component {
+    styles = {
+        root: {},
+        switch: {
+            position: 'relative',
+            display: 'inline-block',
+        },
+    };
+
+    state = {
+        checked: false,
+    };
+
+    onChange = e => {
+        this.setState({ checked: !this.state.checked }, () =>
+            this.props.onChange(e, this.state.checked)
+        );
+    };
+
     render() {
-        const { style, onChange } = this.props;
+        const {
+            theme,
+            style,
+            styles,
+            variant,
+            width,
+            height,
+            onChange,
+            ...props
+        } = this.props;
+
+        const { checked } = this.state;
+
+        const color = theme.colors[variant] || {};
 
         return (
-            <div style={style}>
-                <label className="switch">
-                    <input type="checkbox" onChange={onChange} />
-                    <span className="slider round"></span>
+            <div style={mergeStyles(this.styles.root, styles.root, style)}>
+                <label
+                    style={mergeStyles(
+                        { width, height },
+                        this.styles.switch,
+                        styles.switch
+                    )}
+                >
+                    <Input
+                        type="checkbox"
+                        checked={checked}
+                        onChange={this.onChange}
+                        {...props}
+                    />
+                    <Slider
+                        color={color.light}
+                        checked={checked}
+                        translate={width}
+                        size={height}
+                        borderRadius={height}
+                    />
                 </label>
             </div>
         );
     }
 }
 
-export default Switch;
+Switch.defaultProps = {
+    styles: {},
+    width: 60,
+    height: 32,
+    variant: 'blue',
+    onChange: e => {},
+};
+
+Switch.propTypes = {
+    theme: PropTypes.object.isRequired,
+    styles: PropTypes.object,
+    variant: PropTypes.string,
+    width: PropTypes.any,
+    height: PropTypes.any,
+    onChange: PropTypes.func,
+};
+
+export default withTheme(Switch);
