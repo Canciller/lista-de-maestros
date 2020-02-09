793af7f03bdffd7b27c8c1ccaaa23bdc256fa825
canciller <lopez.ojeda.gabriel.emilio@gmail.com>
2020-02-08 18:36:32 -0600

    Added TextField component

--- src/routes/Home/Home.js
+++ src/routes/Home/Home.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import Typography from 'components/Typography';
+import TextField from 'components/TextField';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 class Home extends React.Component {
     render() {
         return (
-            <div>
-                <Typography component="h1">Inicio</Typography>
+            <div
+                style={{
+                    minHeight: '100%'
+                }}
+            >
+                <TextField
+                    placeholder="Buscar maestro"
+                    icon={faSearch}
+                />
             </div>
         );
     }
