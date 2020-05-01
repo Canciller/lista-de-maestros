5f5052ff263fb1196f5ee525704a2b471027b92d
MauHylian <mauhylian@gmail.com>
2020-05-01 16:08:43 -0500

    Added ReviewForm component

--- src/routes/Testing/Testing.js
+++ src/routes/Testing/Testing.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import Typography from 'components/Typography';
 import View from 'components/View';
+import ReviewForm from 'components/ReviewForm';
 
 class Testing extends React.Component {
+    
+    onSubmit = fields => {
+        console.log('EJEMPLO: ', fields);
+    };
+
     render() {
         return (
             <View>
                 <Typography component="h1">Testing</Typography>
+                <ReviewForm onSubmit={fields => this.onSubmit(fields)}/>
             </View>
         );
     }
